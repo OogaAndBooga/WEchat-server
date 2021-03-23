493d045c0a23f04c48e265ae78015c83bd5e4917
Teofil <teofilboitor@gmail.com>
2021-03-23 17:21:49 +0200

    mere cu salvatul de mesaje

--- wechat-server.py
+++ wechat-server.py
@@ -37,7 +37,7 @@ async def do_stuff(websocket, path):
         for group in groups:
             if username in groups[group]['members']:
                 mygroups.append({group : []}) #the empty list is for messages
-        current_users[username] = {'websocket' : websocket, 'not_requested_groups' : [], 'messages_already_sent' : mygroups}
+        current_users[username] = {'websocket' : websocket, 'requested_groups' : [], 'messages_already_sent' : mygroups}
 
         print('{} connected yaaaaaaaaaaaaaaaaaaaaaaaaaaay'.format(username))
         print('fisst message :') #;libqaelji   EV;OHI  WEFIO;HOIJQWER['OIJ'    E   WGV['OIJ    WGEGV3O;NWDVpoihnvW;ON'WVD'nkWV/LKN]]
@@ -45,8 +45,8 @@ async def do_stuff(websocket, path):
         while True:
             msg = await websocket.recv()
             msg = json.loads(msg)
-            message_type = msg['message_type']
-            if message_type == 'message':
+            action = msg['action']
+            if action == 'message':
                 print('received {} from {}'.format(msg, username))
                 msg['sender'] = username
                 recipient = msg['recipient']
@@ -60,21 +60,34 @@ async def do_stuff(websocket, path):
 
                     for user in groups[recipient]['members']:
                         if user in current_users and user != username:
-                            if recipient in current_users[user]['not_requested_groups']:
+                            if recipient not in current_users[user]['requested_groups']:
                                 current_users[user]['messages_already_sent'][recipient].append(msg) #add messages not to send when requesting messages
                                 print('{} has not requested {}'.format(username, recipient))
                             await current_users[user]['websocket'].send(msg)
                             print('sent {} {}'.format(user, msg))
-            elif message_type == 'request_messages':
-                print('{} requested messages from {}'.format(username, groups[msg['group']]))
-                if username in groups[msg['group']]['members']:
-                    msg = {'messages' : groups[msg['group']]['messages']}
-                    if msg != []:
-                        await websocket.send(json.dumps(msg))
-                        print('sent {} : {}'.format(username, msg))
+            elif action == 'request_messages':
+                requested_group = msg['group']
+                print('{} requested messages from {}, {}'.format(username, requested_group, groups[requested_group]))
+                print('{} has requested groups : {}'.format(username, current_users[username]['requested_groups']))
+                if requested_group not in current_users[username]['requested_groups']:
+                    if username in groups[requested_group]['members']:
+                        msg = {'messages' : list(groups[requested_group]['messages'])}
+                        for i in range(len(msg['messages'])):
+                            if msg['messages'][i]['sender'] == username:
+                                msg['messages'][i]['sender'] = 'me' #pt luca
+                        if msg['messages'] != []:
+                            await websocket.send(json.dumps(msg))
+                            print('sent {} : {}'.format(username, msg))
+                            current_users[username]['requested_groups'].append(requested_group)
+            elif action == 'create_group':
+                print('{} created group {}'.format(username, msg['group']))
+                if msg['group'] not in groups:
+                    groups[msg['group']] = {'members' : [list(people.keys())], 'messages' : []}
+                else:
+                    print('group {} already exists'.format(msg['group']))
     except (excuses.ConnectionClosed, excuses.ConnectionClosedError, excuses.ConnectionClosedOK):
         del current_users[username]
-        print('deleted {}'.format(username))
+        print('deleted {}'.format(username))#remove user from current users
 
 def serve(port):
     start_server = websockets.serve(do_stuff, '0.0.0.0', port)
